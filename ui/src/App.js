4199e87b911a603670786a7092f630ba478cebbc
Cole Sluggett <cole.sluggett@onxmaps.com>
2019-11-13 12:21:11 -0700

    working users and home

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,35 +1,118 @@
 import React, { Component } from "react";
 import "./App.css";
-import UserList from "./UserList";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import gql from "graphql-tag";
-import { useQuery } from '@apollo/react-hooks';
+import { Query } from "react-apollo";
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Navigation } from './Navigation';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
-const GET_USERS = gql`
-    {
-    User{ 
-      id
-      name
-      sex
-    }
+
+class Users extends Component {
+
+  render() {
+    return (
+      <Query
+        query={gql`
+          {
+            User{ 
+              name
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+
+          return (
+            <div>
+              <h3>USERS</h3>
+              <ListGroup>
+                {data.User.map(user => {
+                  return (
+                    <ListGroupItem className="justify-content-between"><Link to={`/user/${user.name}`} className="link">{user.name}</Link></ListGroupItem>
+                  );
+                })}
+              </ListGroup>
+            </div>
+          );
+        }
+        }
+      </Query>);
   }
-`;
+}
 
-function Users() {
-  const { loading, error, data } = useQuery(GET_USERS);
+class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: this.props.name };
+  }
 
-  if (loading) return 'Loading...';
-  if (error) return `Error! ${error.message}`;
-
-  return (
-    <select name="user" >
-      {data.User.map(user => (
-        <h5 key={user.id}>
-          <Link to={`/user/${user.name}`}>{user.name}'s Page</Link>
-        </h5>
-      ))}
-    </select>
-  );
+  render() {
+    return (
+      <Query
+        query={gql`
+              query User($name: String!){
+                User(name: $name, first:1){ 
+                  reviews_received{
+                    stars
+                    text
+                    reviewWriter{
+                      name
+                    }
+                  }
+                  name
+                  sex
+                }
+              }
+          `}
+        variables={{ name: this.state.name }}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+          let userInfo = data.User[0];
+          return (
+            <div>
+              <ul className="list">
+                    <div>
+                      <Card>
+                      <div className="ppContainer">
+                        <CardImg className="profilePic" top src={process.env.PUBLIC_URL + "/img/pp.jpg"} alt="Card image cap" />
+                      </div>
+                        <CardBody>
+                          <CardTitle>{ userInfo.name }</CardTitle>
+                          <CardSubtitle>{ userInfo.sex }</CardSubtitle>
+                          <CardText>
+                            {userInfo.reviews_received.map(review => {
+                              return (
+                                <ul className="List">
+                                  <li><strong>Reviewed By: {review.reviewWriter.name}</strong></li>
+                                  <li>Rating: {review.stars}/5</li>
+                                  <li>Comment: {review.text}</li>
+                                </ul>
+                              );
+                            })
+                          }
+                          </CardText>
+                          <Button>Button</Button>
+                        </CardBody>
+                      </Card>
+                    </div>
+                  );
+              </ul>
+            </div>
+          );
+        }
+        }
+      </Query>
+    );
+  }
 }
 
 const IndexPage = () => {
@@ -47,7 +130,7 @@ const UsersPage = () => {
   return (
     <>
       {
-        Users
+        <Users />
       }
     </>
   );
@@ -57,14 +140,7 @@ const UserPage = ({ match, location }) => {
 
   return (
     <>
-      <p>
-        <strong>User ID: </strong>
-        {name}
-      </p>
-      <p>
-        <strong>User Name: </strong>
-        {Users[name - 1].id}
-      </p>
+      <ProfilePage name={name} />
     </>
   );
 };
@@ -73,25 +149,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img
-            src={process.env.PUBLIC_URL + "/img/logo.png"}
-            className="App-logo"
-            alt="logo"
-          />
-        </header>
-        <Router>
-          <ul className="List">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/users">Users</Link></li>
-          </ul>
-          <Route exact path="/" component={IndexPage} />
-          <Route exact path="/users" component={UsersPage} />
-          <Route exact path="/user/:name" component={UserPage} />
-          <Route exact path="/about" component={AboutPage} />
-        </Router>
-        <UserList />
+        <div className="container-fluid">
+          <div className="row">
+            <main className="main-content col-lg-12 col-md-12 col-sm-12 p-0">
+              <Router>
+                <Navigation />
+                <div class="main-content-container container-fluid px-4">
+                  
+                    <Route exact path="/" component={IndexPage} />
+                    <Route exact path="/users" component={UsersPage} />
+                    <Route exact path="/user/:name" component={UserPage} />
+                  
+                </div>
+            </Router>
+            </main>
+          </div>
+        </div>
 
       </div>
     );
---
+++ ui/src/Navigation.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Link } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavLink,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+
+export class Navigation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/"><img src={process.env.PUBLIC_URL + "/img/logo.png"} height="50px" alt=""></img></NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink tag={Link} to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} to="/Users">Users</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
